3c7cd26bf5c765a6e30aa28f273ab99fc23ad4d6
ari-borlaza <arianeborlaza@gmail.com>
2021-05-27 16:24:19 +0400

    eee

--- lib/src/style/fixed_circle_tab_style.dart
+++ lib/src/style/fixed_circle_tab_style.dart
@@ -54,7 +54,7 @@ class FixedCircleTabStyle extends InnerBuilder {
         width: style.layoutSize,
         height: style.layoutSize,
         decoration: BoxDecoration(
-          border: Border.all(color: d, width: 1),
+          border: Border.all(color: d, width: 2),
           shape: BoxShape.circle,
           color: c,
         ),
