fff05abbf3bee7c447b978bf4ef698c700488af2
ari-borlaza <arianeborlaza@gmail.com>
2021-05-31 16:35:38 +0400

    bb

--- lib/src/style/fixed_circle_tab_style.dart
+++ lib/src/style/fixed_circle_tab_style.dart
@@ -68,7 +68,7 @@ class FixedCircleTabStyle extends InnerBuilder {
         ),
         // margin: EdgeInsets.all(0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             BlendImageIcon(active ? item.activeIcon ?? item.icon : item.icon,
                 size: style.activeIconSize,
